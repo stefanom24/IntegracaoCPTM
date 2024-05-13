793f09a496ccbdd9603666db413ddd4c8c4ff054
stefanom24 <stefanom24@gmail.com>
2024-05-13 10:34:06 -0300

    Finalizado testes de navegacao

--- cypress/e2e/Navegacao/paginas.cy.js
+++ cypress/e2e/Navegacao/paginas.cy.js
@@ -17,17 +17,36 @@ describe('Navegação', () => {
         cy.url().should('include', 'trens')
         
     })
-    
-    /* it('navegar para a página de vídeos', () => {
-        cy.get('#search_form_input_homepage').type('gatos{enter}')
-        cy.get('#zci-videos').click()
-        cy.url().should('include', 'videos')
+
+    it('navegar para a página de cadastro de linhas', () => {
+        cy.get('.navbar-nav').contains('Cadastros').click()
+        cy.get('.dropdown-menu').contains('Cadastro de Linhas').click()
+        cy.url().should('include', 'linhas')
+        
     })
-    
-    it('navegar para a página de notícias', () => {
-        cy.get('#search_form_input_homepage').type('gatos{enter}')
-        cy.get('#zci-news').click()
-        cy.url().should('include', 'news')
+
+    it('navegar para a página de cadastro de estacoes', () => {
+        cy.get('.navbar-nav').contains('Cadastros').click()
+        cy.get('.dropdown-menu').contains('Cadastro de Estações').click()
+        cy.url().should('include', 'estacoes')
+        
     })
-    } */
+
+    it('navegar para a página de registro de ocorrencias de trens', () => {
+        cy.get('.navbar-nav').contains('Registro de Ocorrências').click()
+        cy.get('.dropdown-menu').contains('Ocorrências de Trens').click()
+        cy.url().should('include', 'ocoTrens')
+    })
+
+    it('navegar para a página de registro de ocorrencias de linhas', () => {
+        cy.get('.navbar-nav').contains('Registro de Ocorrências').click()
+        cy.get('.dropdown-menu').contains('Ocorrências de Linhas').click()
+        cy.url().should('include', 'ocoLinhas')
+    })
+
+    it('navegar para a página de registro de ocorrencias de estacoes', () => {
+        cy.get('.navbar-nav').contains('Registro de Ocorrências').click()
+        cy.get('.dropdown-menu').contains('Ocorrências de Estações').click()
+        cy.url().should('include', 'ocoEstacoes')
+    })
 })
